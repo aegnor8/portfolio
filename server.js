4ec66ae7f0e3762156a8b7f3268dc179fbe1ad91
Mattia Carganico <aegnor@hotmail.it>
2023-09-27 19:21:22 -0400

    fourth commit

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 const express = require('./config/express');
+const http = require('http');
 const app = express();
-const port = 3000;
-app.listen(port, () => {
+const port = process.env.PORT || 3000;
+const server = http.createServer(app);
+server.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
 module.exports = app;
