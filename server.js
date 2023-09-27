9bed2d0827eab5c7d7de9eb48da579d08edf9666
Mattia Carganico <aegnor@hotmail.it>
2023-09-26 20:00:42 -0400

    third commit

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-process.env.NODE_ENV = process.env.NODE_ENV || 'development'
-const express = require('./config/express')
-const app = express()
-app.listen(3000)
-module.exports = app
-
-console.log('Server running at http://localhost:3000/')
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+const express = require('./config/express');
+const app = express();
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+module.exports = app;
