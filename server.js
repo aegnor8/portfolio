265d142c01b0138b038c1051b1772f7cdcb08786
Mattia Carganico <aegnor@hotmail.it>
2024-05-02 14:21:42 -0400

    May 24 update

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
-const express = require('./config/express');
-const http = require('http');
+process.env.NODE_ENV = process.env.NODE_ENV || "development";
+const express = require("./config/express");
+const http = require("http");
 const app = express();
 const port = process.env.PORT || 3000;
 const server = http.createServer(app);
